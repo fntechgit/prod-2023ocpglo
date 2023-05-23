7732d23f862d40eb48bd180fd68451790ad4c05c
Jeremy Muir <jeremy@fntech.com>
2023-05-23 13:13:21 -0700

    Update index.js
    updating CMS git commit

--- src/@openeventkit/event-site/cms/config/index.js
+++ src/@openeventkit/event-site/cms/config/index.js
@@ -2,6 +2,6 @@ import defaultConfig from "@openeventkit/event-site/src/cms/config";
 
 const config = { ...defaultConfig };
 // TODO: move to env var
-config.backend.repo = "fntechgit/fnevent-tier1-default-theme";
+config.backend.repo = "fntechgit/prod-2023ocpglo";
 
 export default config;
