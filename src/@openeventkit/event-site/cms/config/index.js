6548f57c1f3c602c92182534f3899852c3d03b68
sebastian marcet <smarcet@gmail.com>
2023-05-26 17:57:05 -0300

    Update index.js

--- src/@openeventkit/event-site/cms/config/index.js
+++ src/@openeventkit/event-site/cms/config/index.js
@@ -3,6 +3,6 @@ import defaultConfig from "@openeventkit/event-site/src/cms/config";
 const config = { ...defaultConfig };
 // TODO: move to env var
 config.backend.repo = "fntechgit/prod-2023ocpglo";
-config.backend.branch = “master”
+config.backend.branch = "master";
 
 export default config;
